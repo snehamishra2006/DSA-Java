bb1d82df974f14aa08ee5c1944bb75e84a4ba27d
snehamishra2006 <snehamishraa522@gmail.com>
2026-06-17 17:04:03 +0530

    Create SortCharByFreq.java

---
+++ HashMap/SortCharByFreq.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+             for(char ch: s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+         }
+         ArrayList<Character> list = new ArrayList<>(map.keySet());
+         //comaparator
+         Collections.sort(list,(a,b)->map.get(b)-map.get(a));
+         StringBuilder sb = new StringBuilder();
+         for(char ch: list){
+            int freq = map.get(ch);
+            for(int i=0; i<freq; i++){
+                sb.append(ch);
+            }
+         } 
+         return sb.toString();
+    }
+}
