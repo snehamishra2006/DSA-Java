6b2316409d23030ab58803164f353d3c867c4fac
snehamishra2006 <snehamishraa522@gmail.com>
2026-06-22 00:32:23 +0530

    Create ContainsDuplicate2.java

---
+++ HashMap/ContainsDuplicate2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+         HashMap<Integer,Integer> map = new HashMap<>();
+         for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int prevIdx = map.get(nums[i]);
+                if(i-prevIdx<=k) return true;
+            }
+            map.put(nums[i],i);
+         }
+         return false;
+    }
+}
