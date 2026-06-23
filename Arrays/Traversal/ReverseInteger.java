39d18c21f08b250bc9c36d90fb079b07d880db26
snehamishra2006 <snehamishraa522@gmail.com>
2026-06-23 20:26:28 +0530

    Create ReverseInteger.java

---
+++ Arrays/Traversal/ReverseInteger.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        if(x==0) return 0;
+        int rev = 0;
+        while(x!=0){
+            int lastDigit = x % 10;
+            if(rev > Integer.MAX_VALUE/10 || rev< Integer.MIN_VALUE/10){
+            return 0;
+            }
+            rev = rev*10 + lastDigit;
+            x = x/10;
+        }
+        return rev;
+    }
+}
