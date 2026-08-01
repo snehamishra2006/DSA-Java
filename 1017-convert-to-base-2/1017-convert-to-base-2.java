4ee5d8849652d9bfcd439b5801d33ff2ed330b27
snehamishra2006 <snehamishraa522@gmail.com>
2026-08-02 00:10:56 +0530

    Time: 1 ms (97.66%), Space: 42.5 MB (81.94%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String baseNeg2(int n) {
+                if (n == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            int rem = n % -2;
+            n = n / -2;
+            if (rem < 0) {
+                rem += 2;
+                n++;
+            }
+            sb.append(rem);
+        }
+        return sb.reverse().toString();
+
+    }
+}
